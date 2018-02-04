2d6f4fd7d8b88d391244459de42a14c397e25c97
at-946 <asato730@gmail.com>
2018-02-04 21:17:39 +0900

    add GA

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # GA tracking ID
+  GA.tracker = "UA-xxxxx-x"
 end
